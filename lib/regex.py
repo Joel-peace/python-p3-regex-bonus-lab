ea8e7fa99d0161f883339dfdf3e72a28c5ed3ec3
Joel <joel.thayu@student.moringaschool.com>
2025-06-02 11:14:59 +0300

    fiinished

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,12 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+my_regex = re.compile(
+    r"(It's such a lovely day today\.|"
+    r"Rainy days make me feel gloomy\.|"
+    r"The sky is clear and the sun is shining\.|"
+    r"Some weather we're having today, huh\?|"
+    r"Maybe today's just not my day\.)"
+)
 
+def find_all_matching_phrases(text):
+    return my_regex.findall(text)
